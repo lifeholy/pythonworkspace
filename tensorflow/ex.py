0c82782135b3eda83763910a9a507ce746bd6c83
lifeholy <lifeholy@naver.com>
2021-12-20 21:04:49 +0900

    20211220 home commit

--- tensorflow/ex.py
+++ tensorflow/ex.py
@@ -1,8 +1,18 @@
-# TensorFlow and tf.keras
-import tensorflow as tf
+import numpy as np 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+num_point = 1000 
+vector_set = [] 
 
-print(tf.__version__)
+for i in range(num_point) : 
+    x1 = np.random.normal(0.0, 0.55) 
+    y1 = x1 * 0.1 + 0.3 + np.random.normal( 0.0, 0.03 ) 
+    vectors_set.append([x1,y1]) 
+    
+x_data = [v[0] for v in vectors_set] 
+y_data = [v[1] for v in vectors_set]
+
+plt.plot(x_data, y_data, 'ro') 
+plt.show()
+
